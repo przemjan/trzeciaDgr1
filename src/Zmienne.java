d1f6a3c6ea6bbf4273602a869fd3698f613d24c1
przemjan <przemjanisz2@gmail.com>
2022-09-13 08:15:49 +0200

    Initial commit

---
+++ src/Zmienne.java
@@ -0,0 +1,29 @@
+public class Zmienne {
+    public static void main(String[] args) {
+        //numerButa mi przechowuje numer buta
+        int numerButa;
+        numerButa = 123;
+
+        double liczba2;
+        liczba2 = 23.9;
+
+        numerButa = (int)liczba2;
+
+        System.out.println("Moja numerButa wynosi " + numerButa);
+
+        int hours = 8;
+        int daysInWeek = 5;
+        int weeks = 52;
+
+        int workHours = hours * daysInWeek * weeks;
+
+        String imie = "Franek \njest \"fajny\"";
+
+        System.out.print("Ilość\tgodzin\\ \n \"pracy\": " + workHours + " \n");
+        System.out.println(imie + ", " + workHours +" "+  weeks);
+
+        char znak = 'g';
+
+        boolean twierdzenie = true; //false
+    }
+}
